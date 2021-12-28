6e680cc490d27c0ca7d94a081077b7b7fe5358c9
jitao <1432599195@qq.com>
2021-12-28 18:08:32 +0800

    初始化项目

---
+++ src/test/java/com/example/myactiviti/MyactivitiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myactiviti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyactivitiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
